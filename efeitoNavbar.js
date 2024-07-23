d98e7ec3e00e4654c276631878746d743fd794c2
entonykwame <147438952+entonykwame@users.noreply.github.com>
2024-07-23 17:21:37 -0300

    Add files via upload

---
+++ efeitoNavbar.js
@@ -0,0 +1,19 @@
+
+function toggleNavbar() {
+    var navbar = document.getElementById("navbar");
+    navbar.classList.toggle("collapsed");
+    var toggleButton = document.querySelector('.toggleButton');
+  
+    
+    if (navbar.classList.contains("collapsed")) {
+      navbar.style.animation = "collapseAnimation 0.3s forwards";
+      toggleButton.style.top = "20%";
+      toggleButton.innerHTML = '&darr;';
+      
+    } else {
+      navbar.style.animation = "expandAnimation 0.3s forwards";
+      navbar.style.visibility = "visible";
+      toggleButton.innerHTML = '&uarr;';
+      toggleButton.style.top = "calc(43% - 50px)";
+    }
+  }
